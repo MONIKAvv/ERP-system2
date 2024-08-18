d026bda3a198dc9cfd6c67daa9e3fe68e06cfb20
Monika-softwaredev35 <monikalonewolf35@gmail.com>
2024-08-18 21:03:09 +0530

    Second Time Testing

---
+++ monika/reducer/studentreducer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function studentreducer() {
+  return (
+    <div>studentreducer
+
+    reducer is here 
+
+    </div>
+
+    
+
+  )
+}
+
+export default studentreducer
